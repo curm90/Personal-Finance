f9c719673f2f4728834af2117e93981912b875c6
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-09-30 10:20:40 +0100

    test adding expense to db from form UI

--- src/components/AddExpenseForm.tsx
+++ src/components/AddExpenseForm.tsx
@@ -1,15 +1,37 @@
+"use client";
+
+import { useActionState } from "react";
+import { Toaster } from "react-hot-toast";
+import addExpense from "@/actions/addExpense";
 import Input from "./Input";
+import FormErrorText from "./FormErrortext";
+
+const initialState = {
+  error: "",
+};
 
 export default function AddExpenseForm() {
+  const [state, action, isPending] = useActionState(addExpense, initialState);
+  console.log({ state, isPending });
+
   return (
     <section className='my-20 flex justify-center'>
       <div className='flex-flex-col'>
-        <form className='flex w-full max-w-[800px] flex-col gap-2 rounded-md border border-gray-300 p-8 shadow-lg'>
+        <form
+          className='flex w-full max-w-[800px] flex-col gap-2 rounded-md border border-gray-300 p-8 shadow-lg'
+          action={action}
+        >
           <h2 className='text-center text-3xl font-semibold text-gray-900'>Add new expense</h2>
           <div className='my-4 h-[1px] w-full bg-gray-200' />
           <div className='flex gap-2'>
-            <Input className='flex-1' type='text' label='Title' />
-            <Input className='flex-1' type='number' label='Amount (£)' />
+            <div className='flex flex-col'>
+              <Input name='title' className='' type='text' label='Title' />
+              {state && state.error && <FormErrorText error={state.error} />}
+            </div>
+            <div className='flex flex-col'>
+              <Input name='amount' className='' type='number' label='Amount (£)' />
+              {state && state.error && <FormErrorText error={state.error} />}
+            </div>
           </div>
           <label className='text-sm font-semibold text-gray-700' htmlFor='descritpion'>
             Description (optional)
@@ -18,16 +40,16 @@ export default function AddExpenseForm() {
             name='description'
             cols={1}
             rows={5}
-            className='border border-solid border-gray-300 px-6 py-2 font-semibold'
+            className='border border-solid border-gray-300 px-4 py-2 font-semibold'
           ></textarea>
-          <Input type='text' label='Notes (optional)' />
-          <Input type='date' label='Date' className='text-gray-700' />
+          <Input name='notes' type='text' label='Notes (optional)' />
           <button
             className='mt-4 w-fit rounded-full bg-sky-300 px-6 py-2 text-center font-semibold text-sky-50 transition hover:bg-sky-200'
             type='submit'
           >
-            Add Expense
+            {isPending ? "Loading..." : "Add Expense"}
           </button>
+          <Toaster />
         </form>
       </div>
     </section>
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,13 +1,22 @@
 import { cn } from "@/lib/utils";
 
 type InputProps = {
-  placeholder?: string;
   type: string;
+  name: string;
+  placeholder?: string;
   className?: string;
   label?: string;
+  required?: boolean;
 };
 
-export default function Input({ placeholder, type, className, label = "" }: InputProps) {
+export default function Input({
+  placeholder,
+  type,
+  name,
+  className,
+  required = false,
+  label = "",
+}: InputProps) {
   return (
     <div className='flex flex-col gap-2'>
       {label && <label className='text-sm font-semibold text-gray-700'>{label}</label>}
@@ -15,6 +24,8 @@ export default function Input({ placeholder, type, className, label = "" }: Inpu
         className={cn("border border-solid border-gray-300 px-4 py-2 font-semibold", className)}
         type={type}
         placeholder={placeholder}
+        required={required}
+        name={name}
       />
     </div>
   );
