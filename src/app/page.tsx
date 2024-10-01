8022bd53dbcdec7164be3414709d6d4084607e90
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-01 10:56:53 +0100

    add logout button in root

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 // import Dashboard from "@/components/Dashboard";
 
-import AddExpenseForm from "@/components/AddExpenseForm";
+import AddExpenseForm from '@/components/AddExpenseForm';
+import { UserButton } from '@clerk/nextjs';
 
 export default function Home() {
   return (
     <div>
       {/* <Dashboard /> */}
+      <UserButton afterSwitchSessionUrl='/' />
       <AddExpenseForm />
     </div>
   );
