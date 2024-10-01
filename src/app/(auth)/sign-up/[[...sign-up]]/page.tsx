b2f614cfc415c73ec42f7de23466d6943abf80d6
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-01 10:56:43 +0100

    style auth pages

--- src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ src/app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,27 @@
-import { SignUp } from "@clerk/nextjs";
+import Image from 'next/image';
+import { Loader2 } from 'lucide-react';
+import { SignUp, ClerkLoaded, ClerkLoading } from '@clerk/nextjs';
 
 export default function SignUpPage() {
-  return <SignUp />;
+  return (
+    <div className='grid min-h-screen grid-cols-1 lg:grid-cols-2'>
+      <div className='h-full flex-col items-center lg:flex'>
+        <div className='space-y-4 pt-16 text-center'>
+          <h1 className='text-3xl font-semibold text-gray-900'>Welcome back</h1>
+          <p className='text-base text-gray-700'>Sign in, or create an account to see your dashboard.</p>
+        </div>
+        <div className='mt-8 flex items-center justify-center'>
+          <ClerkLoaded>
+            <SignUp />
+          </ClerkLoaded>
+          <ClerkLoading>
+            <Loader2 className='text-muted-foreground animate-spin' />
+          </ClerkLoading>
+        </div>
+      </div>
+      <div className='hidden h-full items-center justify-center bg-blue-600 lg:flex'>
+        <Image src='/logo.svg' height={100} width={100} alt='Logo' />
+      </div>
+    </div>
+  );
 }
