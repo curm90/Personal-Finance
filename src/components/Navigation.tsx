e40d7b894ba4dda3702d882eecd4ce6acf205688
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-03 18:14:05 +0100

    add mobile navigation

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,7 +1,13 @@
 'use client';
 
-import { usePathname } from 'next/navigation';
+import { usePathname, useRouter } from 'next/navigation';
+import { useState } from 'react';
+import { useMedia } from 'react-use';
+import { Menu } from 'lucide-react';
+
 import NavButton from './NavButton';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+import { Button } from './ui/button';
 
 const routes = [
   {
@@ -27,7 +33,45 @@ const routes = [
 ];
 
 export default function Navigation() {
+  const [isOpen, setIsOpen] = useState(false);
+
   const pathname = usePathname();
+  const router = useRouter();
+  const isMobile = useMedia('(max-width: 1023px)', false);
+
+  function handleClick(href: string) {
+    setIsOpen(false);
+    router.push(href);
+  }
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant='outline'
+            size='sm'
+            className='border-none bg-white/10 font-normal text-white outline-none transition hover:bg-white/20 hover:text-white focus:bg-white/30 focus-visible:ring-transparent focus-visible:ring-offset-0'
+          >
+            <Menu className='size-4' />
+          </Button>
+        </SheetTrigger>
+        <SheetContent side='left' className='px-2'>
+          <nav className='flex flex-col gap-y-2 pt-6'>
+            {routes.map((route) => (
+              <Button
+                variant={route.href === pathname ? 'secondary' : 'ghost'}
+                key={route.href}
+                onClick={() => handleClick(route.href)}
+              >
+                {route.label}
+              </Button>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+    );
+  }
 
   return (
     <nav className='hidden lg:flex'>
