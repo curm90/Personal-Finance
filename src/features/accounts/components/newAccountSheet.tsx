6e62559c64758ac36beba8ee0d83f9ddd57e4fd7
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-14 18:30:36 +0100

    use mutation on submit, close modal and disable button while pending mutation

--- src/features/accounts/components/newAccountSheet.tsx
+++ src/features/accounts/components/newAccountSheet.tsx
@@ -1,22 +1,29 @@
 import { useNewAccount } from '@/features/accounts/hooks/useNewAccount';
+import useCreateAccount from '@/features/accounts/api/use-create-account';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription } from '@/components/ui/sheet';
 import AccountForm, { type FormValues } from './AccountForm';
 
 export default function NewAccountSheet() {
   const { isOpen, onClose } = useNewAccount();
 
+  const mutation = useCreateAccount();
+
   function handleSubmit(values: FormValues) {
-    console.log({ values });
+    mutation.mutate(values, {
+      onSuccess: () => {
+        onClose();
+      },
+    });
   }
 
   return (
-    <Sheet open={!isOpen} onOpenChange={onClose}>
+    <Sheet open={isOpen} onOpenChange={onClose}>
       <SheetContent className='space-y-4'>
         <SheetHeader>
           <SheetTitle>New Account</SheetTitle>
           <SheetDescription>Add a new account to your list</SheetDescription>
         </SheetHeader>
-        <AccountForm defaultValues={{ name: '' }} onSubmit={handleSubmit} disabled={false} />
+        <AccountForm defaultValues={{ name: '' }} onSubmit={handleSubmit} disabled={mutation.isPending} />
       </SheetContent>
     </Sheet>
   );
