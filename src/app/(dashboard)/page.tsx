2aa78028c59df6004a82a94bb874cce062890d55
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-14 17:05:13 +0100

    add clerk middleware and only get accounts from authorized user

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,11 +1,3 @@
-'use client';
-
-import useGetAccounts from '@/features/accounts/api/use-get-accounts';
-
 export default function DashboardPage() {
-  const { data: accounts, isLoading } = useGetAccounts();
-
-  if (isLoading) return <div>Loading...</div>;
-
-  return <div>{accounts?.map((account) => <div key={account.id}>{account.name}</div>)}</div>;
+  return <div>Dashboard page</div>;
 }
