c38ffd1fd5e548d5961d007f065cda6272c71760
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-15 11:23:38 +0100

    render actual db data and add loading ui

--- src/app/(dashboard)/accounts/page.tsx
+++ src/app/(dashboard)/accounts/page.tsx
@@ -1,30 +1,36 @@
 'use client';
 
-import { Plus } from 'lucide-react';
+import { Loader2, Plus } from 'lucide-react';
 
 import { useNewAccount } from '@/features/accounts/hooks/useNewAccount';
+import useGetAccounts from '@/features/accounts/api/use-get-accounts';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Button } from '@/components/ui/button';
 import { DataTable } from '@/components/ui/data-table';
-import { Payment, columns } from './columns';
-
-const data: Payment[] = [
-  {
-    id: '728ed52f',
-    amount: 100,
-    status: 'pending',
-    email: 'm@example.com',
-  },
-  {
-    id: '728ed52f',
-    amount: 50,
-    status: 'success',
-    email: 'a@example.com',
-  },
-];
+import { Skeleton } from '@/components/ui/skeleton';
+import { columns } from './columns';
 
 export default function AccountsPage() {
   const { onOpen } = useNewAccount();
+  const accountsQuery = useGetAccounts();
+  const accounts = accountsQuery.data || [];
+
+  if (accountsQuery.isLoading) {
+    return (
+      <div className='-m-24 mx-auto w-full max-w-screen-2xl pb-10'>
+        <Card className='border-none drop-shadow-sm'>
+          <CardHeader>
+            <Skeleton className='h-8 w-48' />
+          </CardHeader>
+          <CardContent>
+            <div className='flex h-[500px] items-center justify-center'>
+              <Loader2 className='size-6 animate-spin text-slate-300' />
+            </div>
+          </CardContent>
+        </Card>
+      </div>
+    );
+  }
 
   return (
     <div className='-m-24 mx-auto w-full max-w-screen-2xl pb-10'>
@@ -36,7 +42,7 @@ export default function AccountsPage() {
           </Button>
         </CardHeader>
         <CardContent>
-          <DataTable columns={columns} data={data} filterKey='email' />
+          <DataTable columns={columns} data={accounts} filterKey='email' />
         </CardContent>
       </Card>
     </div>
