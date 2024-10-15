351f87b38155c84d9289337ff3ce5eb7a0fa99dd
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-15 12:35:42 +0100

    use edit account hook to update data in edit account sheet

--- src/features/accounts/components/EditAccountSheet.tsx
+++ src/features/accounts/components/EditAccountSheet.tsx
@@ -1,25 +1,27 @@
 import { Loader2 } from 'lucide-react';
-import useCreateAccount from '@/features/accounts/api/use-create-account';
 import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetDescription } from '@/components/ui/sheet';
 import AccountForm, { type FormValues } from './AccountForm';
 import { useOpenAccount } from '../hooks/useOpenAccount';
 import useGetAccount from '../api/use-get-account';
+import useEditAccount from '../api/use-edit-account';
 
 export default function EditAccountSheet() {
   const { isOpen, onClose, id } = useOpenAccount();
 
   const accountQuery = useGetAccount(id);
-  const mutation = useCreateAccount();
+  const editAccountMutation = useEditAccount(id);
+
+  const isLoading = accountQuery.isLoading;
+  const isPending = editAccountMutation.isPending;
 
   function handleSubmit(values: FormValues) {
-    mutation.mutate(values, {
+    editAccountMutation.mutate(values, {
       onSuccess: () => {
         onClose();
       },
     });
   }
 
-  const isLoading = accountQuery.isLoading;
   const defaultValues = accountQuery.data ? { name: accountQuery.data.name } : { name: '' };
 
   return (
@@ -34,12 +36,7 @@ export default function EditAccountSheet() {
             <Loader2 className='size-4 animate-spin' />
           </div>
         ) : (
-          <AccountForm
-            id={id}
-            defaultValues={defaultValues}
-            onSubmit={handleSubmit}
-            disabled={mutation.isPending}
-          />
+          <AccountForm id={id} defaultValues={defaultValues} onSubmit={handleSubmit} disabled={isPending} />
         )}
       </SheetContent>
     </Sheet>
