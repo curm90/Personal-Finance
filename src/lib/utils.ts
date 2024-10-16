c94b236ef39234fa8c3f0ae2bcfb07c29b4b9a4a
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-16 17:07:39 +0100

    add helper fn to format currency to usd

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -12,3 +12,11 @@ export function convertAmountToMilliUnits(amount: number) {
 export function convertAmountFromMilliUnits(amount: number) {
   return amount / 1000;
 }
+
+export function formatCurrency(amount: number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 2,
+  }).format(amount);
+}
