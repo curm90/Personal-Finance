afdd549375725c0f85147b887ff78bc024ce3e93
Liam Sutton <liam_sutton1990@hotmail.co.uk>
2024-10-16 16:50:34 +0100

    add helper fns to convert amount

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,22 +1,14 @@
-import { type ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function getErrorMessage(error: unknown): string {
-  let message = "";
+export function convertAmountToMilliUnits(amount: number) {
+  return Math.round(amount * 1000);
+}
 
-  if (error instanceof Error) {
-    message = error.message;
-  } else if (error && typeof error === "object" && "message" in error) {
-    message = String(error.message);
-  } else if (typeof error === "string") {
-    message = error;
-  } else {
-    message = "Something went wrong";
-  }
-
-  return message;
+export function convertAmountFromMilliUnits(amount: number) {
+  return amount / 1000;
 }
